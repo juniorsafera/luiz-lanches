ad5e11b95fbdafbfe3ed40b8345b9d728c4056e2
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-16 09:58:48 -0300

    Update main.dart

--- luiz_lanches/lib/main.dart
+++ luiz_lanches/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:luiz_lanches/others/rotas.dart';
 import 'package:luiz_lanches/views/tela_inicial.dart';
+import 'package:luiz_lanches/views/tela_principal.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: TelaInicial(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        // ignore: prefer_const_constructors
+        Rotas.home: (context) => TelaInicial(),
+        // ignore: prefer_const_constructors
+        Rotas.telaprincipal: (context) => TelaPrincipal(),
+        // ignore: prefer_const_constructors
+      },
+    ),
+  );
 }
