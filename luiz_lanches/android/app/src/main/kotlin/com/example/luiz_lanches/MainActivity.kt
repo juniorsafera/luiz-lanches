6d704fbeae7d1f460580771bf31ccfc84ee97b65
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-15 23:37:57 -0300

    criando projeto

---
+++ luiz_lanches/android/app/src/main/kotlin/com/example/luiz_lanches/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.luiz_lanches
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ luiz_lanches/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:luiz_lanches/views/tela_inicial.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: TelaInicial(),
+  ));
+}
