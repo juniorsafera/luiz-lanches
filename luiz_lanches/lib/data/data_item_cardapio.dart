2ac696af40061fae4c60f5dc99ed96a28184ef11
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-16 09:58:51 -0300

    Create data_item_cardapio.dart

---
+++ luiz_lanches/lib/data/data_item_cardapio.dart
@@ -0,0 +1,110 @@
+import 'package:luiz_lanches/models/item_cardapio.dart';
+
+// ignore: unnecessary_const, constant_identifier_names
+const DADOS_ITEM_CARDAPIO = const [
+  ModelItemCardapio(
+      codigo: '001',
+      titulo: 'X-BURGUER',
+      itens: ['pão', 'hambúrguer', 'maionese artesenal', 'queijo', 'presunto'],
+      categoria: 'hamburguer',
+      valor: '14'),
+  ModelItemCardapio(
+      codigo: '002',
+      titulo: 'X-SALADA',
+      itens: [
+        'pão',
+        'hambúrguer',
+        'maionese artesenal',
+        'queijo, presunto, alface, tomate, milho, ervilha, batata palha'
+      ],
+      categoria: 'hamburguer',
+      valor: '18'),
+  ModelItemCardapio(
+      codigo: '003',
+      titulo: 'X-EGG',
+      itens: [
+        'pão',
+        'hambúrguer',
+        'ovo',
+        'maionese artesenal',
+        'queijo',
+        'presunto',
+        'alface',
+        'tomate',
+        'milho',
+        'ervilha',
+        'batata'
+      ],
+      categoria: 'hamburguer',
+      valor: '20'),
+  ModelItemCardapio(
+      codigo: '004',
+      titulo: 'X-CALABRESA',
+      itens: [
+        'pão',
+        'hambúrguer',
+        'calabresa',
+        'maionese artesenal',
+        'queijo',
+        'presunto',
+        'alface',
+        'tomate',
+        'milho',
+        'ervilha',
+        'batata'
+      ],
+      categoria: 'hamburguer',
+      valor: '21'),
+  ModelItemCardapio(
+      codigo: '005',
+      titulo: 'X-BACON',
+      itens: [
+        'pão',
+        'hambúrguer',
+        'bacon',
+        'maionese artesenal',
+        'queijo',
+        'presunto',
+        'alface',
+        'tomate',
+        'milho',
+        'ervilha',
+        'batata'
+      ],
+      categoria: 'hamburguer',
+      valor: '22'),
+  ModelItemCardapio(
+      codigo: '006',
+      titulo: 'X-GALINHA',
+      itens: [
+        'pão',
+        'filé de frango',
+        'maionese artesenal',
+        'queijo',
+        'presunto',
+        'alface',
+        'tomate',
+        'milho',
+        'ervilha',
+        'batata'
+      ],
+      categoria: 'hamburguer',
+      valor: '14'),
+  ModelItemCardapio(
+      codigo: '007',
+      titulo: 'X-FILÉ',
+      itens: [
+        'pão',
+        'filé',
+        'maionese artesenal',
+        'queijo',
+        'presunto',
+        'alface',
+        'tomate',
+        'milho',
+        'ervilha',
+        'batata'
+      ],
+      categoria: 'hamburguer',
+      valor: '14'),
+];
