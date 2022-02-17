054b42a02da7ea003e86927823e8a001f5597237
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-17 17:36:33 -0300

    Create botaoadd.dart

---
+++ luiz_lanches/lib/components/botaoadd.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:luiz_lanches/others/paleta_cores.dart';
+
+class BotaoAdd extends StatelessWidget {
+  final String valor;
+  const BotaoAdd({
+    Key? key,
+    required this.valor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 40,
+      child: Row(
+        children: [
+          Container(
+              decoration: const BoxDecoration(
+                color: Paleta.corPrimaria,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(0),
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(0),
+                ),
+              ),
+              width: 80,
+              height: 40,
+              child: Stack(
+                children: [
+                  const Positioned(
+                      top: 3,
+                      left: 12,
+                      child: Text(
+                        'R\$',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 11,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Text(
+                      valor,
+                      style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                    ),
+                        )),
+                  ),
+                ],
+              )),
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(0),
+                topRight: Radius.circular(25),
+                bottomLeft: Radius.circular(0),
+                bottomRight: Radius.circular(25),
+              ),
+            ),
+            width: 50,
+            height: 40,
+            child: const Center(
+                child: Text(
+              '+add',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
