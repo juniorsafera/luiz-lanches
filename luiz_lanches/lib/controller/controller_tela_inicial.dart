72924c40c8cfbbe4e0c46e5075d46953ac070e9b
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-16 22:36:32 -0300

    Update controller_tela_inicial.dart

--- luiz_lanches/lib/controller/controller_tela_inicial.dart
+++ luiz_lanches/lib/controller/controller_tela_inicial.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-class ControllerTelaInicial{
-       abrirTelaPrincipal(BuildContext context) {
-    Navigator.of(context)
-        .pushNamed('/telaprincipal');
+class ControllerTelaInicial {
+  // MÉTODO PARA ABRIR A TELA PRINCIPAL
+  abrirTelaPrincipal(BuildContext context) {
+    Navigator.of(context).pushNamed('/telaprincipal');
   }
+
+     
 }
