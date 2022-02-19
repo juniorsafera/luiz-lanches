0d094345b00d7fbff6e7bbfadfa3ebd4479173d6
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-18 21:20:05 -0300

    Create data_item_adicional.dart

---
+++ luiz_lanches/lib/data/data_item_adicional.dart
@@ -0,0 +1,12 @@
+import 'package:luiz_lanches/models/item_adicional.dart';
+
+const DADOS_ITENS_ADICIONAIS = [
+  ModelItemAdicional(codigo: 'a1', titulo: 'Calabresa', valor: '1.00'),
+  ModelItemAdicional(codigo: 'a2', titulo: 'Bacon', valor: '1.00'),
+  ModelItemAdicional(codigo: 'a3', titulo: 'Salada', valor: '1.00'),
+  ModelItemAdicional(codigo: 'a4', titulo: 'Queijo', valor: '1.00'),
+  ModelItemAdicional(codigo: 'a5', titulo: 'Presunto', valor: '1.00'),
+  ModelItemAdicional(codigo: 'a6', titulo: 'Tomate', valor: '1.00'),
+  ModelItemAdicional(codigo: 'a7', titulo: 'Pepino', valor: '1.00'),
+  ModelItemAdicional(codigo: 'a8', titulo: 'Ovo', valor: '1.00'),
+];
