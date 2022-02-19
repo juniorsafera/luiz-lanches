d1a0a812d0a66ba028620047f39791feec2b000f
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-19 10:31:03 -0300

    Update frame_add_carrinho.dart

--- luiz_lanches/lib/components/frame_add_carrinho.dart
+++ luiz_lanches/lib/components/frame_add_carrinho.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:luiz_lanches/components/lista_itens_adicionais.dart';
 import 'package:luiz_lanches/data/data_item_adicional.dart';
@@ -16,9 +15,50 @@ class FrameAddCarrinho {
               title: Center(
                   child: Text('Adicionar ${itemTitulo}  R\$ ${valor},00')),
               content: SizedBox(
-                height: size.height * 0.5,
+                height: size.height * 0.6,
                 child: Column(
                   children: [
+                    // Definindo label de input (Observações)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: const Text(
+                              "Observações:",
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            )),
+
+                        // Definindo   input (Observações)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Expanded(
+                              child: Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.grey)),
+                                child: const TextField(
+                                  maxLines: 3,
+                                  decoration: InputDecoration.collapsed(
+                                    hintText: " ",
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                      child: Container(),
+                    ),
+                    // Definindo área de adicionais
                     const Padding(
                       padding: EdgeInsets.all(8.0),
                       child: Text(
