1fd8a0551b4cc65151ec3e6ea327d3f5be7a643d
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-18 21:20:16 -0300

    Update item_cardapio.dart

--- luiz_lanches/lib/models/item_cardapio.dart
+++ luiz_lanches/lib/models/item_cardapio.dart
@@ -4,11 +4,14 @@ class ModelItemCardapio {
   final List<String> itens;
   final String categoria;
   final String valor;
+  final bool disponivel;
 
-  const ModelItemCardapio(
-      {required this.codigo,
-      required this.titulo,
-      required this.itens,
-      required this.categoria,
-      required this.valor});
+  const ModelItemCardapio({
+    required this.codigo,
+    required this.titulo,
+    required this.itens,
+    required this.categoria,
+    required this.valor,
+    this.disponivel = true,
+  });
 }
