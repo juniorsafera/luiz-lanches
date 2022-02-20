a468d9a1f4889a9e9b2d0ab9c4780f1428b5f07b
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-20 10:07:12 -0300

    Create tela_carrinho.dart

---
+++ luiz_lanches/lib/views/tela_carrinho.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaCarrinho extends StatelessWidget {
+   
+  const TelaCarrinho({ Key? key,
+  
+   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children:const [
+      Text('Carrinho')
+    ],);
+  }
+}
