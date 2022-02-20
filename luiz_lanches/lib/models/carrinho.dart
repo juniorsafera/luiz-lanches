00512025a43d2006ceff2569afa65901329e899d
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-20 10:07:28 -0300

    Create carrinho.dart

---
+++ luiz_lanches/lib/models/carrinho.dart
@@ -0,0 +1,31 @@
+import 'package:luiz_lanches/models/pedido.dart';
+
+class ModelCarrinho {
+  final String codigo;
+  final List<ModelPedido> pedidos;
+  final String tipoPagamento;
+  final bool troco;
+  final bool entrega;
+  final String endereco;
+  final String numero;
+  final String bairro;
+  final String contato;
+  final String observacao;
+  final  String valorEntrega;
+  final String valorTotalCarrinho;
+
+  const ModelCarrinho({
+    required this.codigo,
+    required this.pedidos,
+    required this.tipoPagamento,
+    required this.troco,
+    required this.entrega,
+    required this.endereco,
+    required this.numero,
+    required this.bairro,
+    required this.contato,
+    required this.observacao,
+      this.valorEntrega = '5,00',
+    required this.valorTotalCarrinho
+  });
+}
