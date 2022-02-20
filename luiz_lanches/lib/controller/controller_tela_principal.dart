abdc531c91e34a1255dc1faeea45779108b78c32
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-20 17:16:46 -0300

    Update controller_tela_principal.dart

--- luiz_lanches/lib/controller/controller_tela_principal.dart
+++ luiz_lanches/lib/controller/controller_tela_principal.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:luiz_lanches/models/carrinho.dart';
 
 class ControllerTelaPrincipal {
-  abrirTelaCarrinho(BuildContext context, List<ModelCarrinho> dCarrinho) {
-    Navigator.of(context)
-        .pushNamed('/telacarrinho', arguments: dCarrinho as ModelCarrinho);
+  abrirTelaCarrinho(BuildContext context) {
+    Navigator.of(context).pushNamed('/telacarrinho');
   }
 }
