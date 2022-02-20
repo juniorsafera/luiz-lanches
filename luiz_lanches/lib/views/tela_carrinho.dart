ec38fc61d597306b07fa34da209d732200f2f512
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-20 12:39:51 -0300

    Update tela_carrinho.dart

--- luiz_lanches/lib/views/tela_carrinho.dart
+++ luiz_lanches/lib/views/tela_carrinho.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:luiz_lanches/models/carrinho.dart';
 
-class TelaCarrinho extends StatelessWidget {
-   
-  const TelaCarrinho({ Key? key,
-  
-   }) : super(key: key);
+class TelaCarrinho extends StatefulWidget {
+  const TelaCarrinho({
+    Key? key,
+  }) : super(key: key);
 
   @override
+  State<TelaCarrinho> createState() => _TelaCarrinhoState();
+}
+
+class _TelaCarrinhoState extends State<TelaCarrinho> {
+  @override
   Widget build(BuildContext context) {
-    return Column(children:const [
-      Text('Carrinho')
-    ],);
+    final dCarrinho =
+        ModalRoute.of(context)!.settings.arguments as ModelCarrinho;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('dCarrinho.nomeCliente'),
+      ),
+      body: Column(
+        children: [Text('dCarrinho.bairro')],
+      ),
+    );
   }
 }
