553fe1c3c52f5f398cf1d63e77e70ad2a8e75da7
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-20 20:28:20 -0300

    Update controller_tela_principal.dart

--- luiz_lanches/lib/controller/controller_tela_principal.dart
+++ luiz_lanches/lib/controller/controller_tela_principal.dart
@@ -1,8 +1,27 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:luiz_lanches/data/data_pedido.dart';
 import 'package:luiz_lanches/models/carrinho.dart';
+import 'package:luiz_lanches/models/pedido.dart';
 
 class ControllerTelaPrincipal {
+final dados = DADOS_PEDIDOS.toList();
+
   abrirTelaCarrinho(BuildContext context) {
     Navigator.of(context).pushNamed('/telacarrinho');
+  }
+
+  adicionarPedido(String pedido, List<String> adicionais, String observacao,
+      String valorTotalItem) {
+    final novoPedido = ModelPedido(
+        codigo: Random().nextDouble().toString(),
+        pedido: pedido,
+        adicionais: adicionais,
+        valorTotalItem: valorTotalItem);
+
+    
+    dados.add(novoPedido);
+    print('OK: ${dados.toList()}');
   }
 }
