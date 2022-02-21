e7543b8b839fb873a016207e0c8604fd1f1c2033
Junior Safera <juniorsafera.dev@gmail.com>
2022-02-21 17:45:40 -0300

    Update data_pedido.dart

--- luiz_lanches/lib/data/data_pedido.dart
+++ luiz_lanches/lib/data/data_pedido.dart
@@ -1,3 +1,21 @@
-import 'package:luiz_lanches/models/pedido.dart';
+import 'dart:math';
 
-const List<ModelPedido> DADOS_PEDIDOS = [];
+import 'package:luiz_lanches/models/pedido.dart';
+import 'package:luiz_lanches/views/tela_carrinho.dart';
+
+class DadosPedido{
+    final List<ModelPedido> dadosPedidos = [];
+
+    void adicionarPedido(String pedido, List<String> adicionais, String valorTotalItem){
+          dadosPedidos.add(
+            ModelPedido(codigo: Random().nextDouble().toString(),
+            pedido: pedido, 
+            adicionais: adicionais, 
+            valorTotalItem: valorTotalItem),
+            );
+            print(dadosPedidos[0].pedido);
+            final TelaCarrinho dca = TelaCarrinho();
+            
+             
+    }
+}
